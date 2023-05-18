e8f47c3d4a4ae73e3a80b8f7d5f164db6bddee98
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-19 02:40:05 +0600

    create api and create api by id

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const coursesApi = require("./data/coursesData.json");
+
 app.get("/", (req, res) => {
   res.send("Server is running");
+});
+
+app.get("/allCoursesApi/", (req, res) => {
+  res.send(coursesApi);
+});
+
+app.get("/allCoursesApi/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const api = coursesApi.find((apiById) => apiById.id === id);
+  res.send(api);
 });
 
 app.listen(port, () => {
