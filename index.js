fe50e6f0626bf22b6289b9e957c3d0077e6b821b
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-20 14:03:13 +0600

    add img link from internent

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 const coursesApi = require("./data/coursesData.json");
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
